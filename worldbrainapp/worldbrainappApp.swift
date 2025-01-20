fac7a9ade87b05ef79e47b5578bf4b827a9bd19d
worldbrain <juncokevin@@hotmail.com>
2025-01-20 10:21:03 -0600

    Initial Commit

---
+++ worldbrainapp/worldbrainappApp.swift
@@ -0,0 +1,17 @@
+//
+//  worldbrainappApp.swift
+//  worldbrainapp
+//
+//  Created by Daniel on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct worldbrainappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ worldbrainappTests/worldbrainappTests.swift
@@ -0,0 +1,17 @@
+//
+//  worldbrainappTests.swift
+//  worldbrainappTests
+//
+//  Created by Daniel on 20/01/2025.
+//
+
+import Testing
+@testable import worldbrainapp
+
+struct worldbrainappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
